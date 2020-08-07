f5d0da25300472d6bbb5a56ab6668a0988956689
anxigy <gy010806@gmail.com>
2020-08-07 19:18:00 +0900

    Updates

---
+++ precache-manifest.ff59c8ee3145cfb38bf52137e8b4512e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab9428852b186f924d5e7e10902511a",
+    "url": "/bday/index.html"
+  },
+  {
+    "revision": "0c562adfa335658b5209",
+    "url": "/bday/static/css/main.020e056c.chunk.css"
+  },
+  {
+    "revision": "9eddebdcfbd2faee6815",
+    "url": "/bday/static/js/2.d6c5a952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bday/static/js/2.d6c5a952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c562adfa335658b5209",
+    "url": "/bday/static/js/main.32add529.chunk.js"
+  },
+  {
+    "revision": "a40c8dce5b5641c0c7ac",
+    "url": "/bday/static/js/runtime-main.4f47d400.js"
+  },
+  {
+    "revision": "298fd31969423e017cb290077c55ceba",
+    "url": "/bday/static/media/04_illust.298fd319.png"
+  },
+  {
+    "revision": "8faba3db5e37f5f5e3bb9e10cb6a74aa",
+    "url": "/bday/static/media/Gotham-Black.8faba3db.ttf"
+  },
+  {
+    "revision": "1883dc031bedc202202a001b9ba1020b",
+    "url": "/bday/static/media/NotoSansCJKkr-Light.1883dc03.otf"
+  },
+  {
+    "revision": "2ec8557460d3a2cd7340b16ac84fce32",
+    "url": "/bday/static/media/Raleway-Medium.2ec85574.ttf"
+  },
+  {
+    "revision": "b419bbc01e483207ade2aecd3707add2",
+    "url": "/bday/static/media/fail-1.b419bbc0.jpeg"
+  },
+  {
+    "revision": "ea26b56017786d49121e503d256090db",
+    "url": "/bday/static/media/fail-2.ea26b560.jpeg"
+  },
+  {
+    "revision": "0d2327badb245421d528da2782d25c5e",
+    "url": "/bday/static/media/fail-3.0d2327ba.png"
+  },
+  {
+    "revision": "5c84bf0ca1df5811604716c2df167b61",
+    "url": "/bday/static/media/fail-4.5c84bf0c.png"
+  },
+  {
+    "revision": "4cd4465f66ce105019278e37d5419b12",
+    "url": "/bday/static/media/img.4cd4465f.png"
+  },
+  {
+    "revision": "7ece0cde2907cb4be34e49389379b9a8",
+    "url": "/bday/static/media/img_photo history.7ece0cde.png"
+  },
+  {
+    "revision": "c225b6fd4fe471b0ca07c13a8716a9e9",
+    "url": "/bday/static/media/logo.c225b6fd.png"
+  },
+  {
+    "revision": "e1ae5fdd8eb571523b74c207a211d158",
+    "url": "/bday/static/media/phoca.e1ae5fdd.png"
+  }
+]);
